052a662d58e71c6b2449d48642c12bafd556197e
Carrie Barrett <me.carriebarrett@gmail.com>
2022-07-27 14:53:09 -0700

    home page retrieves affirmations
    fixed home page to use a StreamBuilder to grab 3 random affirmations. Still randomizes every time page reloads. Also did some refactoring to separate widgets into separate files and cleaned up code smells on imports

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -2,57 +2,56 @@ import 'package:flutter/material.dart';
 
 import '../constants.dart';
 import '../screens/all_affirmations.dart';
-import '../screens/homepage.dart';
+import '../screens/new_homepage.dart';
 
 Widget drawer(BuildContext context) {
   return Drawer(
-    backgroundColor: middleBlueGreen,
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: [
-        const DrawerHeader(
-            decoration: BoxDecoration(
-              color: darkSlateBlue,
-            ),
-            child: Text('Navigation')),
-        ListTile(
-          title: const Text('Home'),
-          onTap: () {
-            // update state
-            // close drawer
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        const MyHomePage(title: appTitle)));
-          },
-        ),
-        ListTile(
-          title: const Text('Favorites'),
-          onTap: () {
-            // update state
-            // close drawer
-          },
-        ),
-        ListTile(
-          title: const Text('All Affirmations'),
-          onTap: () {
-            // update state
-            // close drawer
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        const AllAffirmationsPage(title: appTitle)));
-          },
-        ),
-        ListTile(
-          title: const Text('Preferences'),
-          onTap: () {
-            // update state
-            // close drawer
-          },
-        )
-      ],
-    ));
+      backgroundColor: middleBlueGreen,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: darkSlateBlue,
+              ),
+              child: Text('Navigation')),
+          ListTile(
+            title: const Text('Home'),
+            onTap: () {
+              // update state
+              // close drawer
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const HomePage(title: appTitle)));
+            },
+          ),
+          ListTile(
+            title: const Text('Favorites'),
+            onTap: () {
+              // update state
+              // close drawer
+            },
+          ),
+          ListTile(
+            title: const Text('All Affirmations'),
+            onTap: () {
+              // update state
+              // close drawer
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const AllAffirmationsPage(title: appTitle)));
+            },
+          ),
+          ListTile(
+            title: const Text('Preferences'),
+            onTap: () {
+              // update state
+              // close drawer
+            },
+          )
+        ],
+      ));
 }
